f1f44d77d6e02da2e58c487b58ff036ff4d647ea
vapvin <tpehdj12@daum.net>
2019-11-14 23:16:04 +0900

    User Mixins

--- users/mixins.py
+++ users/mixins.py
@@ -1,5 +1,17 @@
-from django.shortcuts import redirect, reverse
-from django.contrib.auth.mixins import UserPassesTestMixin
+from django.contrib import messages
+from django.urls import reverse_lazy
+from django.shortcuts import redirect
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
+
+
+class EmailLoginOnlyView(UserPassesTestMixin):
+
+    def test_func(self):
+        return self.request.user.login_method == "email"
+
+    def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
+        return redirect("core:home")
 
 
 class LoggedOutOnlyView(UserPassesTestMixin):
@@ -10,4 +22,9 @@ class LoggedOutOnlyView(UserPassesTestMixin):
         return not self.request.user.is_authenticated
 
     def handle_no_permission(self):
+        messages.error(self.request, "Can't go there")
         return redirect("core:home")
+
+
+class LoggedInOnlyView(LoginRequiredMixin):
+    login_url = reverse_lazy("users:login")
