4b0b5a9c8a335cbb4a24b83de0618507410df04d
vapvin <tpehdj12@daum.net>
2020-01-24 21:39:29 +0900

    This Complete

--- lists/urls.py
+++ lists/urls.py
@@ -5,5 +5,5 @@ from . import views
 app_name = "lists"
 
 urlpatterns = [
-    path("add/<int:room_pk>", views.save_room, name="save-room")
+    path("toggle/<int:room_pk>", views.toggle_room, name="toggle-room")
 ]
