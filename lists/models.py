3b32e56404c53fad9d9636f616d5038047782490
vapvin <tpehdj12@daum.net>
2020-01-24 21:29:13 +0900

    Fav Links Edit

--- lists/models.py
+++ lists/models.py
@@ -7,10 +7,11 @@ class List(core_models.TimeStampedModel):
     """ List Model Definition """
 
     name = models.CharField(max_length=80)
-    user = models.ForeignKey(
+    user = models.OneToOneField(
         "users.User", related_name="lists", on_delete=models.CASCADE
     )
-    rooms = models.ManyToManyField("rooms.Room", related_name="lists", blank=True)
+    rooms = models.ManyToManyField(
+        "rooms.Room", related_name="lists", blank=True)
 
     def __str__(self):
         return self.name
--- lists/views.py
+++ lists/views.py
@@ -1,10 +1,10 @@
 from django.shortcuts import redirect, reverse
-from rooms import models
+from rooms import models as room_models
 from . import models
 
 
 def save_room(request, room_pk):
-    room = models.Room.objects.get_or_none(pk=room_pk)
+    room = room_models.Room.objects.get_or_none(pk=room_pk)
     if room is not None:
         the_list, _ = models.List.objects.get_or_create(
             user=request.user, name="My Favourites Houses"
